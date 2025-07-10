2b073647bae992244c252c06ee513c75a684e4b4
Atharva <souliconicpanda123@gmail.com>
2025-07-10 21:51:40 +0530

    increment and decrement samplez

---
+++ increment_decreament.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+    printf(" the value of i is : %d \n", i);
+
+    i = i + 5;
+    printf(" the value of i is : %d\n", i);
+
+    i++;
+
+     i = i - 16;
+    printf(" the value of i is : %d", i);
+
+    --i;
+
+    return 0;
+}

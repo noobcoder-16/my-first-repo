a9aa3ece848a8091fdffa92991c0c46f5d971fc1
Atharva <souliconicpanda123@gmail.com>
2025-07-10 20:46:15 +0530

    while_loop sample

---
+++ while_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i = 0;
+
+    while (i < 7)
+    {
+        printf("pretty little baby , beedi lelo baby\n");
+    
+
+    i = i + 1;
+    }
+
+    return 0;
+}

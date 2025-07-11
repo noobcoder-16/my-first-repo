2e112d32faf94730d8d827e6828f055fc87d7297
Atharva <souliconicpanda123@gmail.com>
2025-07-11 13:50:21 +0530

    increment_decreament

---
+++ do_while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    do
+    {
+
+        printf(" the value of i is : %d \n", i);
+           i++;
+
+
+    } /* code */
+    while (i < 7);
+
+     
+    return 0;
+}

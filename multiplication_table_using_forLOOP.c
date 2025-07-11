d7c837432cb86a08472c314526ec3beb2211482d
Atharva <souliconicpanda123@gmail.com>
2025-07-11 22:34:23 +0530

    multiplication_table_using_forLOOP

---
+++ multiplication_table_using_forLOOP.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+
+    printf(" enter the nummber : ");
+    scanf("%d", &n);
+
+    for (int i = 0; i <=10; i++)
+
+    {
+
+        printf("%dX%d=%d\n", n, i, n * i);
+       
+    }
+
+    return 0;
+}

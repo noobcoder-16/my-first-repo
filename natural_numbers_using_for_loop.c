c25a8499f3bac9f71cfc2471d79428b8009a3f55
Atharva <souliconicpanda123@gmail.com>
2025-07-11 17:34:20 +0530

    natural number using for loop

---
+++ natural_numbers_using_for_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 10;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        printf(" the first natural numbers are : %d\n", i);
+    }
+    return 0;
+}
